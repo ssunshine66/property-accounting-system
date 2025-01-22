1c26bb70c81ad70f46943b3f61baebe4829f3697
Vladyslav Korniienko <vkornienko63@gmail.com>
2025-01-22 23:50:46 +0200

    Added Automapper profile

---
+++ BLL/Mappers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.Entities;
+
+namespace BLL.Mappers;
+
+public class MappingProfiles: Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Report, ReportRequestDto>().ReverseMap();
+        CreateMap<Report, RegisterRepairDto>().ReverseMap();
+        CreateMap<User, CreateUserDto>().ReverseMap();
+        CreateMap<FinancialRecord, FinancialRecordDto>().ReverseMap();
+    }
+}
