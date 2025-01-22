f9f37df13c77c7adfd6cac8a77c4095174e7b430
Vladyslav Korniienko <vkornienko63@gmail.com>
2025-01-22 23:48:30 +0200

    UnitOfWork used

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
+    Task SaveChangesAsync();
+    IEquipmentRepository Equipments { get; }
+    IFinancialRecordRepository FinancialRecords { get; }
+    IMaintenanceRepository Maintenance { get; }
+    IReportRepository Reports { get; }
+    IUserRepository Users { get; }
+}
